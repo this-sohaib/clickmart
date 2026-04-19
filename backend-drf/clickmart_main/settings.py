e1c016d8d5144612f7ea15bdfc93b46d36761d96
Sohaib Hassan <sohaibhassan712@gmail.com>
2026-04-19 09:28:43 +0500

    frontend + backend updates

--- backend-drf/clickmart_main/settings.py
+++ backend-drf/clickmart_main/settings.py
@@ -173,4 +173,7 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'http://localhost:5174',
+    'http://127.0.0.1:5174',
 ]
